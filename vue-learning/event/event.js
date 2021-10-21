4ff132da3bf04ea8a9c0a5e5759f72847d454183
Chris Wong <wh18710455626@126.com>
2021-10-21 19:34:15 +0800

    events

---
+++ vue-learning/event/event.js
@@ -0,0 +1,31 @@
+/**
+ * const app = Vue.createApp({
+    data() {
+        return {
+            name: 'Vue.js'
+        }
+    },
+    methods: {
+        greet(event) {
+            alert('Hello' + this.name + '!');
+            if (event) {
+                alert(event.target.tagName)
+            }
+        }
+    }
+}).mount('#event-with-method')
+*/
+
+
+
+//...
+// methods: {
+//     one(event) {
+//         //
+//     },
+//     two(event) {
+//         //
+//     }
+// },
+
+
