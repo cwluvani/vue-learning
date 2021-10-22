ee707bdb97004c3bafdbb0be4cd289ad7df6d6bc
Chris Wong <wh18710455626@126.com>
2021-10-22 09:56:55 +0800

    SFC

---
+++ vue-learning/SFC/about.vue
@@ -0,0 +1,29 @@
+<template>
+    <p class="greeting">{{ greeting }}</p>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            greeting: 'hello world!'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+    $greetingColor: red;
+    $greetingFontWeight: bold;
+
+    .greeting {
+        color: $greetingColor;
+        font-weight: $greetingFontWeight;
+    }
+
+
+</style>
+
+
